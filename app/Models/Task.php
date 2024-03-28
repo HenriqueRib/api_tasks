be25ea5434f74bdbb1f79db36c6a0de763389141
HenriqueRib <ribeiro.henriquem@gmail.com>
2024-03-27 23:29:25 -0300

    feat create tasks database

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,9 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'user_id',
+        'active',
+        'name',
+        'description',
+        'deadline',
+        'status', // 0: A fazer, 1: Em andamento, 2: Concluído
+        'priority', // 0: Baixa, 1: Média, 2: Alta
+        'tag',
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
 }
+
+
--- database/migrations/2024_03_27_224214_create_tasks_table.php
+++ database/migrations/2024_03_27_224214_create_tasks_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->nullable();
+            $table->integer('active')->nullable()->default(1)->comment('0: Desativado, 1: Ativado');
+            $table->string('name')->nullable();
+            $table->longText('description')->nullable();
+            $table->date('deadline')->nullable();
+            $table->integer('status')->nullable()->comment('0: A fazer, 1: Em andamento, 2: Concluído');
+            $table->integer('priority')->nullable()->comment('0: Baixa, 1: Média, 2: Alta');
+            $table->string('tag')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
